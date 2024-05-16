e591da99daa05d450fb1216b456c6a82b32631f6
ForeverOptimizing <rlowder94@gmail.com>
2024-05-16 00:43:49 -0700

    lecture 1 start

--- cs50/mario-more/mario.c
+++ cs50/mario-more/mario.c
@@ -4,6 +4,6 @@
 #include <stdio.h>
 
 int main(void) {
-    
+        printf("Hello, World!\n");
 
 }
--- cs50/me/hello.c
+++ cs50/me/hello.c
@@ -4,5 +4,8 @@
 #include <stdio.h>
 
 int main(void) {
+
     printf("Hello, World!\n");
+
+
 }
